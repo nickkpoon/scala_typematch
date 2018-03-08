0fdf78e6d47a792c66868a73a811ea657308bcc8
nickkpoon <nickkpoon@gmail.com>
2018-03-07 21:28:29 -0800

    App working

--- src/main/scala/TypeChecker.scala
+++ src/main/scala/TypeChecker.scala
@@ -17,7 +17,8 @@ object TypeChecker {
   def typeOf(prog: Expr, env: Map[String, ExprType]): ExprType = prog match
     {
 
-
+    //case Var(a) => typeOf(a, Map(a, ExprType))
+    case Var(name) => env(name)
     case Nat(_)  => Nat
     case Bool(_) => Bool
 
@@ -27,24 +28,61 @@ object TypeChecker {
     case App(a, b)
       if (typeOf(a, env) == Nat) && typeOf(b, env) == Bool =>*/
 
-    case Inl(a, b)
-      if typeOf(a, env) == Nat => Sum(Nat,Bool)
-    case Inl(a, b)
-      if typeOf(a, env) == Bool => Sum(Bool, Nat)
-    case Inr(a, b)
-      if typeOf(a, env) == Nat => Sum(Bool, Nat)
-    case Inr(a, b)
-      if typeOf(a, env) == Bool => Sum(Nat, Bool)
+    case Inl(a, b) => Sum(typeOf(a, env),b)
+    /*case Inl(a, b)
+      if typeOf(a, env) == Bool => Sum(Bool, b)*/
+    case Inr(a, b) => Sum(b, typeOf(a, env))
 
-    case Fst(a)
-      if typeOf(a, env) == Prod(Nat, Bool) => Nat
-    case Fst(a)
-      if typeOf(a, env) == Prod(Bool, Nat) => Bool
+    case App(a, b) => typeOf(a, env) match
+    {
+      case Fun(c, d) =>
+      {
+          if (typeOf(b, env) == c)
+            Fun(typeOf(a, env), typeOf(b, env))
+          else
+          throw TypeException("App Error 1!")
 
-    case Snd(a)
-      if typeOf(a, env) == Prod(Nat, Bool) => Bool
-    case Snd(a)
-      if typeOf(a, env) == Prod(Bool, Nat) => Nat
+      }
+      case _ => throw TypeException("App Error 2!")
+
+    }
+
+     /* if (typeOf(a, env) != typeOf(b, env))
+        throw TypeException("App Error!")
+      else
+        Fun(typeOf(a, env), typeOf(b, env))*/
+    /*case Inr(a, b)
+      if typeOf(a, env) == Bool => Sum(Nat, Bool)*/
+
+    /*case Fst(a) => Prod(typeOf(a, env), _)
+      if typeOf(a, env) == Prod(typeOf(a, env), _) => typeOf(a, env)*/
+
+    case Pair(a,b) => Prod(typeOf(a, env), typeOf(b, env))
+
+    case Fst(a) => typeOf(a, env)
+    match
+    {
+      case Prod(c, d) => c
+      case _ => throw TypeException("Fst Error!")
+    }
+    /*case Fst(a)
+      if typeOf(a, env) == Prod(Bool, _) => Bool*/
+
+    case Snd(a) => typeOf(a, env)
+      match
+      {
+      case Prod(c, d) => d
+      case _ => throw TypeException("Snd Error!")
+    }
+    /*case Snd(a)
+      if typeOf(a, env) == Prod(Bool, Nat) => Nat*/
+    /*case Lam(a, b, c)
+      if typeOf(c, env) == Fun(_, typeOf(c, env)) => typeOf(c, env)*/
+
+    //case Pair(a, b) => Prod(typeOf(a, env), typeOf(b, env))
+
+    case Lam(a,b,c) => Fun(b, typeOf(c, env + (a->b)))
+    //case Var(string) =>
 
 
 
